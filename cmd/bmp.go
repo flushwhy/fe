985c875155c35fcd28fb5dd8a2e644bb0db76069
Flushwhy <cars6155@gmail.com>
2024-04-03 11:47:31 -0400

    Step one

--- cmd/bmp.go
+++ cmd/bmp.go
@@ -1,11 +1,12 @@
 /*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2024 Ryan Flush <roflush@pm.me>
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
@@ -20,11 +21,15 @@ var bmpCmd = &cobra.Command{
     You do have to be signed into the CLI inorder for this to work.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-        username := cmd.Flag("username").Value.String()
-        game := cmd.Flag("game").Value.String()
-        directory := cmd.Flag("directory").Value.String()
-        fmt.Print(username, game, directory)
+		username := cmd.Flag("username").Value.String()
+		game := cmd.Flag("game").Value.String()
+		directory := cmd.Flag("directory").Value.String()
 
+		if directory == "" {
+			directory, _ = os.Getwd()
+		}
+
+		bulter_pusher(username, game, directory)
 	},
 }
 
@@ -32,6 +37,29 @@ func init() {
 	rootCmd.AddCommand(bmpCmd)
 
 	bmpCmd.Flags().String("username", "", "itch.io username")
-    bmpCmd.Flags().String("game", "", "itch.io game")
-    bmpCmd.Flags().String("directory", "", "Directory to export folder")
+	bmpCmd.Flags().String("game", "", "itch.io game")
+	bmpCmd.Flags().String("directory", "", "Directory to export folder")
+}
+
+func bulter_pusher(username, game, directory string) {
+	fmt.Println("Pushing " + directory + " to " + username + "/" + game)
+
+	dirs, err := filepath.Glob(filepath.Join(directory + "*"))
+	if err != nil {
+		fmt.Println("Error reading export directory: ", err)
+		return
+	}
+
+	for _, dir := range dirs {
+		fileInfo, err := os.Stat(dir)
+		if err != nil {
+			fmt.Println("Error reading export directory: ", err)
+			continue
+		}
+
+		if fileInfo.IsDir() {
+			fmt.Println("Pushing to " + username + "/" + game + "/" + fileInfo.Name())
+		}
+	}
+
 }
